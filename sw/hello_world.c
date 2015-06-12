ea9ba9be007f59d5a2d9cb0c925b234a6a7e3de8
Daniel Vatanabe Pazinato <ra116545@iron.lab.ic.unicamp.br>
2015-06-12 11:25:52 -0300

    fix asm call in hello_world.c and opcode of ll and sc in mips_isa.ac

--- sw/hello_world.c
+++ sw/hello_world.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef mutex int *;
+//typedef mutex int*;
 
 //return 1 when gets the lock
-int mutex_acquire(mutex m) {
+int mutex_acquire(int *m) {
 
 	int lock;
+	int temp;
+
 	while (*m != 0) {
-     asm (
-				//   rt    rs
-				"ll  %r7, 0(%0)\n"
-				"sc $1, 0(%0)"
-				: "r" (m) //input rs %0
-				: "=r" (lock)//output %1
-			);
+		temp = 0;
+				 														//%0 ouput    %1 input
+		asm volatile ("ll %0, 0(%1)" : "=r" (temp) : "r" (m));
+																		//%0 output    %1 input
+		asm volatile ("sc %0, 0(%1)" : "=r" (lock) : "r" (m));
 
-			if (lock) {
-				//got lock
-				return 1;
-			} 
+		if (!temp && lock) {
+			//got lock
+			return 1;
+		} 
+		//wait a little bit until ask for mutex again
+		int i;
+		for (i = 0; i < 100; ++i); 
 	}
-
+	
 }
 
-int mutex_release(mutex m) {
+int mutex_release(int* m) {
 	*m = 0;
 }
 
 
 int main(int argc, char *argv[]){
   int i;
-  for(i=0;i<10;i++)
+  int m = 0;
+  for(i=0;i<10;i++) {
+  	mutex_acquire(&m);
     printf("Hi from processor MIPS!\n");
+    //mutex_release(&m)
+  }
 
   exit(0); // To avoid cross-compiler exit routine
   return 0; // Never executed, just for compatibility
